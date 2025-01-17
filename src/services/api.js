94eb109aee2eb4d80ff3257742a58dae2e5af757
Host-Adi <145929913+spillproof98@users.noreply.github.com>
2025-01-17 22:18:30 +0530

    Updates

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:3001/tasks';
-
-export const fetchTasks = async () => axios.get(API_URL);
-export const createTask = async (task) => axios.post(API_URL, task);
-export const updateTask = async (id, updatedTask) => axios.put(`${API_URL}/${id}`, updatedTask);
-export const deleteTask = async (id) => axios.delete(`${API_URL}/${id}`);
